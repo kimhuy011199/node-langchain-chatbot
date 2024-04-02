c4e70e70576547e4e4c537782de48b80315bc83f
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-04-02 11:36:37 +0700

    Handle timout error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes';
+import timeout from 'connect-timeout';
 import 'dotenv/config';
 import 'express-async-errors';
 import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
 const port = 8080;
+
+// Handle timeout
+app.use(timeout('10s'));
+app.use((req, res, next) => {
+  if (!req.timedout) {
+    next();
+  }
+});
 
 app.use(cors());
 app.use(express.json());
